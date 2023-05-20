5e1c470b73de2e74a6652e709ed0e7d5419a01e7
prashanthi <it20613754@my.sliit.lk>
2023-05-20 08:32:32 +0530

    Modified app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import logo from "./logo.svg";
 import AdminList from "./components/electricityManagement/List";
-import AddElectricityType from "./components/electricityManagement/AddForm";
+import AddWaterType from "./components/electricityManagement/AddForm";
 
-import ElectricityConnectionForm from "./components/electricityManagement/ElectricityConnectionForm";
-import EditElectricityType from "./components/electricityManagement/EditForm";
+import WaterConnectionForm from "./components/electricityManagement/WaterConnectionForm";
+import EditWaterType from "./components/electricityManagement/EditForm";
 import SingleCard from "./components/electricityManagement/singleCard.js";
 function App() {
   return (
@@ -14,9 +14,9 @@ function App() {
         {/* <Route path="/" element={<Home />} /> */}
         <Route path="/" element={<SingleCard />} />
         <Route path="/adminList" element={<AdminList />} />
-        <Route path="/addEleTyp" element={<AddElectricityType />} />
-        <Route path="/connectionForm" element={<ElectricityConnectionForm />} />
-        <Route path="/editEleTyp/:id" element={<EditElectricityType />} />
+        <Route path="/addWatTyp" element={<AddWaterType />} />
+        <Route path="/connectionForm" element={<WaterConnectionForm />} />
+        <Route path="/editWatTyp/:id" element={<EditWaterType />} />
       </Routes>
     </Router>
   );
